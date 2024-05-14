c86288320c826cb402076f08777842f71f15d9ab
Lamarr Paul <Lamarr_paul97@outlook.com>
2024-05-14 11:46:16 +0100

    created goal model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_13_222913) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_14_085732) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,17 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_13_222913) do
     t.float "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "goals", force: :cascade do |t|
+    t.string "title"
+    t.float "amount"
+    t.string "notes"
+    t.date "date_by"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_goals_on_user_id"
   end
 
   create_table "incomes", force: :cascade do |t|
@@ -54,4 +65,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_13_222913) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "goals", "users"
 end
