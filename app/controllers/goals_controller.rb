c014b644fcebc6ea1bae9a38dcbd09b9975beceb
Lamarr Paul <Lamarr_paul97@outlook.com>
2024-05-14 17:48:35 +0100

    created investment controller and coded the goals controller

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,2 +1,21 @@
 class GoalsController < ApplicationController
+  def new
+    @goal = Goal.new
+  end
+
+  def create
+    @goal = Goal.new(goal_params)
+    @goal.user = current_user
+    if @goal.save
+      
+    else
+    end
+
+  end
+
+  private
+
+  def goal_params
+    params.require(:goal).permit(:title, :amount, :notes, :date_by)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_14_085732) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_14_162556) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_14_085732) do
     t.string "first_name"
     t.string "last_name"
     t.string "username"
+    t.float "pension_contribution"
+    t.float "yearly_bonus"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
