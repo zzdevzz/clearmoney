bfee19082a5c85d0c035eeb4be3fc1280fe3e996
dev <dev.h@hotmail.co.uk>
2024-05-18 22:40:49 +0100

    tests still wip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,13 @@
 class UsersController < ApplicationController
-  
+  # def show
+  #   user = User.find(params[:id])
+  #   @example = user.investments
+  # end
+
+  def index
+    @item = User.where(user: current_user)
+    respond_to do |format|
+      format.html { render partial: "shared/index", locals: { attribute: @item }, layout: false }
+    end
+  end
 end
