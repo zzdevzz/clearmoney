96661cd612caf8e6502257dd1f92465219506640
dev <dev.h@hotmail.co.uk>
2024-05-19 21:57:54 +0100

    styling update

--- db/migrate/20240515194503_create_investments.rb
+++ db/migrate/20240515194503_create_investments.rb
@@ -3,9 +3,10 @@ class CreateInvestments < ActiveRecord::Migration[7.1]
     create_table :investments do |t|
       t.string :name
       t.float :buy_price
-      t.integer :quantity
-      t.float :amount
       t.date :buy_date
+      t.float :current_price
+      t.float :amount
+      t.integer :quantity
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
