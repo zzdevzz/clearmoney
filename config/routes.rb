4fbe1cb1af859f6ded057e80c74cd18c450f216b
dev <dev.h@hotmail.co.uk>
2024-05-20 22:04:04 +0100

    wave added

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  require "sidekiq/web"
+  authenticate :user, ->(user) { user.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 
   # Defines the root path route ("/")
   # root "posts#index"
@@ -28,4 +32,6 @@ Rails.application.routes.draw do
   resources :savings, only: [ :new, :create, :index, :show, :destroy]
   resources :incomes, only: [ :new, :create, :index, :show, :destroy]
   resources :users, only: [:show]
+
+
 end
